a179dfc66fbd5a2ac01fd6aaac3977e05bbf4be6
ahmedismail <ahmed.ismail.1627@gmail.com>
2024-01-08 01:56:41 +0200

    add morgan logger middleware

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import express from 'express'
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv'
-
+import morgan from 'morgan'
 export const app = express()
 
 dotenv.config()
 app.use(bodyParser.urlencoded({ extended: false }))
+
+if (process.env.NODE_ENV !== 'production') {
+  app.use(morgan('dev'))
+
+}
 
 
 
